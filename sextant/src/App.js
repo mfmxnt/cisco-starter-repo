70806e3d4c7d4a6c2c0f4d57d2be81c35017fa0b
Manuel Montalvo Garcia <mfmg@live.unc.edu>
2023-08-06 00:17:49 -0400

    ip addresses

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,14 +1,21 @@
+import React, { Component } from 'react';
 import './App.css';
+import Banner from './Banner';
+import Exhibit from './Exhibit';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Sextant Dashboard
-      </header>
-      <div className="App-exhibit">Exhibit Component</div>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Banner bannerText="Sextant" />
+                <Exhibit name="One"></Exhibit>
+                <Exhibit name="Two"></Exhibit>
+                <Exhibit name="Three"></Exhibit>
+                <IPContainer ipv6={false} /> {/* Display IPv4 address */}
+                <IPContainer ipv6={true} />  {/* Display IPv6 address */}
+            </div>
+        );
+    }
 }
 
 export default App;
